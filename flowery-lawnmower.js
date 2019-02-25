baf8e6fcef012b6363a67d9992ca0efe3e18e19d
PEXY98 <47343648+PEXY98@users.noreply.github.com>
2019-02-25 11:13:42 -0800

    Create flowery-lawnmower.js

---
+++ flowery-lawnmower.js
@@ -0,0 +1,35 @@
+function setup() {
+  createCanvas(400, 200);
+  colorMode(HSB);
+}
+
+var x = 0;
+var h = 10;
+
+function draw() {
+  stroke(random(60, 70), 100, 90);
+  line(x, height-10, x+random(0, 10), height-10-random(h));
+  noStroke();
+
+  x = x + 10;
+
+  if (x > width) {
+    x = random(10);
+    h = h + 3;
+  }
+  
+  if (h >= 30) {
+    noStroke();
+    fill(random(100), 100, 100);
+    ellipse(random (400), random (height - 18, height - 25) , 5);
+  }
+
+  if (h >= 50) {
+    fill(255);
+    rect(0, 0, width, height-15);
+    h = 10;
+  }
+
+  fill(40, 100, 60);
+  rect(0, height-10, width, 10);
+}
